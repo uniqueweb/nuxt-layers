8889182ced79fcdd4e8dbc6522bc9e58c604ffb3
Maximilian Schwarz <maximilian@unique-web.de>
2025-11-01 13:25:40 +0100

    Change test 2

--- password-protection/components/HelloWorld.vue
+++ password-protection/components/HelloWorld.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-const { myLayer } = useAppConfig()
 </script>
 
 <template>
   <div>
-    <h1>Hello World! change my layer</h1>
-    <pre>{{ myLayer }}</pre>
+    <h1>Hello World!</h1>
   </div>
 </template>
